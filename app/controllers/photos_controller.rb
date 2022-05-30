ab60db195b61c16f83b3bf208942ca331eec8df1
Dayton-Killian-1 <dayton.killian@chicagobooth.edu>
2022-05-30 21:20:10 +0000

    Tabs working with bootstrap 5

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -57,6 +57,11 @@ class PhotosController < ApplicationController
     end
   end
 
+  def liked
+    @liked_photos = Photo.find
+  end
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_photo
